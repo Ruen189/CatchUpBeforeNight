179b11f8020ca85a03067d84c1fc2aba6ca54efb
rosti <rostisav-gordeev-2016@mail.ru>
2024-04-14 20:52:20 +0500

    first

---
+++ CatchUpBeforeNight1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace CatchUpBeforeNight1
+{
+    partial class View
+    {
+
+
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View));
+            this.character = new System.Windows.Forms.PictureBox();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // character
+            // 
+            this.character.BackColor = System.Drawing.Color.Transparent;
+            this.character.Image = ((System.Drawing.Image)(resources.GetObject("character.Image")));
+            this.character.Location = new System.Drawing.Point(0, 0);
+            this.character.Name = "character";
+            this.character.Size = new System.Drawing.Size(134, 157);
+            this.character.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.character.TabIndex = 2;
+            this.character.TabStop = false;
+            this.character.Click += new System.EventHandler(this.character_Click);
+            // 
+            // background
+            // 
+            this.background.BackColor = System.Drawing.Color.Transparent;
+            this.background.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("background.BackgroundImage")));
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(1152, 1102);
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            this.background.Click += new System.EventHandler(this.background_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1152, 1102);
+            this.Controls.Add(this.character);
+            this.Controls.Add(this.background);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "View";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox character;
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CatchUpBeforeNight1/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+using WMPLib;
+namespace CatchUpBeforeNight1
+{
+    // view
+    public partial class View : Form
+    {
+        private GameController controller;
+        private WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public View()
+        {
+            InitializeComponent();
+            PlayMusic();
+            SetVolume(10);
+            controller = new GameController(this); // Инициализируем controller перед подпиской на события, шоб не было ошибки треклятой
+            this.KeyDown += new KeyEventHandler(OnKeyDown);
+            this.KeyUp += new KeyEventHandler(OnKeyUp);
+        }
+        private void PlayMusic()
+        {
+
+            player.URL = @"C:\Users\rosti\Downloads\Astron - credits song for my death but im the final boss..mp3";
+            //player.settings.setMode("loop", true);
+            player.controls.play();
+
+        }
+
+        private void SetVolume(int volume)
+        {
+            // Проверка на допустимый диапазон громкости от 0 до 100
+            if (volume < 0) volume = 0;
+            if (volume > 100) volume = 100;
+
+            player.settings.volume = volume;
+        }
+        class MyPanel : Panel { public MyPanel() { this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true); } }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                controller.HandleKeyDown(e);
+                e.Handled = true;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            controller.HandleKeyUp(e);
+        }
+
+        public void UpdateCharacterPosition(Point position)
+        {
+            character.Location = position;
+            this.Refresh();
+        }
+        /*
+        public void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            controller.OnKeyPress(e);
+        }
+        */
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void character_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class GameModel
+    {
+        public Point CharacterPosition { get; set; }
+        public bool LeftPressed { get; set; } // булевые флаги для отслеживания клавиш
+        public bool RightPressed { get; set; }
+        public bool UpPressed { get; set; }
+        public bool DownPressed { get; set; }
+
+        public GameModel()
+        {
+            CharacterPosition = new Point(0, 0); // Начальное положение персонажа
+        }
+
+        public void MoveCharacter(int dx, int dy)
+        {
+            CharacterPosition = new Point(CharacterPosition.X + dx, CharacterPosition.Y + dy);
+        }
+    }
+
+    class GameController
+    {
+        private GameModel model;
+        private View view;
+        private Timer moveTimer;
+        private bool leftPressed = false;
+        private bool rightPressed = false;
+        private bool upPressed = false;
+        private bool downPressed = false;
+
+        public GameController(View view)
+        {
+            this.view = view;
+            this.model = new GameModel();
+
+            moveTimer = new Timer();
+            moveTimer.Interval = 5; // Частота обновления
+            moveTimer.Tick += (sender, e) => MoveCharacter();
+        }
+
+        public void HandleKeyDown(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A: model.LeftPressed = true; break;
+                case Keys.D: model.RightPressed = true; break;
+                case Keys.W: model.UpPressed = true; break;
+                case Keys.S: model.DownPressed = true; break;
+            }
+
+            moveTimer.Start();
+        }
+
+        public void HandleKeyUp(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A: model.LeftPressed = false; break;
+                case Keys.D: model.RightPressed = false; break;
+                case Keys.W: model.UpPressed = false; break;
+                case Keys.S: model.DownPressed = false; break;
+            }
+
+            if (!model.LeftPressed && !model.RightPressed && !model.UpPressed && !model.DownPressed)
+                moveTimer.Stop();
+        }
+
+        private void MoveCharacter()
+        {
+            var speed = 5;
+            var diagonalSpeed = speed / Math.Sqrt(2);
+
+            var dx = 0; 
+            var dy = 0;
+
+            if (model.LeftPressed) dx -= speed;
+            if (model.RightPressed) dx += speed;
+            if (model.UpPressed) dy -= speed;
+            if (model.DownPressed) dy += speed;
+
+            // Движется ли персонаж по диагонали
+            if ((model.LeftPressed || model.RightPressed) && (model.UpPressed || model.DownPressed))
+            {
+                dx = (int)(Math.Sign(dx) * diagonalSpeed);
+                dy = (int)(Math.Sign(dy) * diagonalSpeed);
+            }
+
+            model.MoveCharacter(dx, dy);
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            view.UpdateCharacterPosition(model.CharacterPosition);
+        }
+    }
+
+
+    /*
+    public partial class Form1 : Form, IView
+    {
+        private GameController controller;
+
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new GameController(this, character, background);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            controller.HandleKeyPress(e.KeyCode);
+        }
+
+        public void UpdateCharacterPosition(Point newPosition)
+        {
+            character.Location = newPosition;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public interface IView
+    {
+        void UpdateCharacterPosition(Point newPosition);
+    }
+
+    public class GameController
+    {
+        private const int CharacterSpeed = 5;
+        private const int WorldSize = 1152;
+        private IView view;
+        private PictureBox character;
+        private PictureBox background;
+
+        public GameController(IView view, PictureBox character, PictureBox background)
+        {
+            this.view = view;
+            this.character = character;
+            this.background = background;
+        }
+
+        public void HandleKeyPress(Keys key)
+        {
+            int newX = character.Location.X;
+            int newY = character.Location.Y;
+
+            switch (key)
+            {
+                case Keys.W:
+                    newY -= CharacterSpeed;
+                    break;
+                case Keys.S:
+                    newY += CharacterSpeed;
+                    break;
+                case Keys.A:
+                    newX -= CharacterSpeed;
+                    break;
+                case Keys.D:
+                    newX += CharacterSpeed;
+                    break;
+            }
+
+            newX = (newX + WorldSize) % WorldSize;
+            newY = (newY + WorldSize) % WorldSize;
+
+            Point newPosition = new Point(newX, newY);
+            view.UpdateCharacterPosition(newPosition);
+        }
+    }
+    */
+}
+
+/*
+ * private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void background_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+*/
